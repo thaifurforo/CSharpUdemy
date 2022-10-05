e78e527bd604f2ba7116f1db9162f58fbbcc78be
thainara.furforo <thainara.furforo@stone.com.br>
2022-10-05 10:28:30 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 18. Saída de dados em C# Exercício

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+            
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
+            Console.WriteLine($"\n Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"\n Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F2", CultureInfo.InvariantCulture)}");
+            
+        }
+    }
+}
