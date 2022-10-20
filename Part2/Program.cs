1617f765485de1b743d4a00d3819555f25b6bde9
Thainara Furforo <thaifurforo@gmail.com>
2022-10-20 17:30:25 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 22. Entrada de dados em C# - Parte 2

---
+++ Part2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            Console.WriteLine($"{nome} do sexo {sexo} tem {idade} anos e {altura}m de altura");
+        }
+    }
+}
