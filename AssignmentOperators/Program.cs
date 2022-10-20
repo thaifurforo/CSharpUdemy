66b570dc97795c60ccfb5420aa36a1c3ab2c8a89
Thainara Furforo <thaifurforo@gmail.com>
2022-10-20 00:09:50 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 19. Operadores de atribuição

---
+++ AssignmentOperators/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AssignmentOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            a /= 2;
+            Console.WriteLine(a);
+
+            a %= 4;
+            Console.WriteLine(a);
+
+            a -= 1;
+            Console.WriteLine(a);
+
+            a++;
+            ++a;
+            Console.WriteLine(a);
+
+            a--;
+            --a;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int x = 10;
+            int y = x++;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            int xx = 10;
+            int yy = ++xx;
+            Console.WriteLine(xx);
+            Console.WriteLine(yy);
+        }
+    }
+} 
