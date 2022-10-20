d0ad1262bce9b5a2731ed226e533c1e191b52a46
Thainara Furforo <thaifurforo@gmail.com>
2022-10-20 00:25:55 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 20. Conversão implícita e casting

---
+++ CastingAndConversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CastingAndConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float x = 4.5f;
+            double y = x;
+            // Conversão implícita entre tipos
+            Console.WriteLine(y);
+
+            double a = 5.1;
+            float b = (float)a;
+            Console.WriteLine(a);
+            // Casting
+
+            int p = 5;
+            int q = 2;
+            double resultado = p / q;
+            Console.WriteLine(resultado);
+            
+            resultado = (double)p / q;
+            Console.WriteLine(resultado);
+            // Necessário fazer casting senão ele considera que int / int = int, mesmo que a variável seja double 
+
+        }
+    }
+
+}
