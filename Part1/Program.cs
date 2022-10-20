b3c9200920f52c5dad5a763f750a56b6635c584d
Thainara Furforo <thaifurforo@gmail.com>
2022-10-20 13:41:25 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 22. Entrada de dados em C# - Parte 1

---
+++ Part1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            Console.WriteLine($"Você digitou {frase}, {x}, {y} e {z}");
+
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+            string[] vet2 = Console.ReadLine().Split(' ');
+            for (int i = 0; i < vet2.Length; i++)
+            {
+                Console.WriteLine(vet2[i]);
+            }
+
+
+        }
+    }
+}
