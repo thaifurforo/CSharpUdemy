1a63e07260e765fa80122f55909be96cf4eb78d1
Thainara Furforo <thaifurforo@gmail.com>
2022-10-21 00:06:44 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 26. Operadores Lógicos

--- LogicOperators/Program.cs
+++ LogicOperators/Program.cs
@@ -16,6 +16,18 @@ namespace LogicOperators
             bool c3 = 4 != 5 && !(2 > 3);
             Console.WriteLine(c3);
 
+            Console.WriteLine("-----------------");
+
+            bool c4 = c2 || c3 && c1;
+            // c3 && c1 = false
+            // false || c2 (true) = true
+            Console.WriteLine(c4);
+            
+            bool c5 = (c2 || c3) && c1;
+            // c2 || c3 = true
+            // true && c1 (false) = false
+            Console.WriteLine(c5);
+
         }
     }
 }
