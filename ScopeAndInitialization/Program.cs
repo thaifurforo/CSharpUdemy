da43f33a394c3f2600c5672d5a2109339e3fbceb
Thainara Furforo <thaifurforo@gmail.com>
2022-10-24 15:41:31 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 28. Escopo e inicialização

---
+++ ScopeAndInitialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ScopeAndInitialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço:");
+            double preco = double.Parse(Console.ReadLine());
+
+            double desconto;
+
+            if (preco > 100.0)
+                desconto = preco * 0.1;
+            else
+                desconto = preco * 0.05;
+
+            Console.WriteLine($"O desconto é de {desconto.ToString("C2", CultureInfo.CurrentCulture)}");
+            
+        
+        }
+    }    
+}
+
