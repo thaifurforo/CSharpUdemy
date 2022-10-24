cb811aa40091d7ca191a023db9ad668a8288833a
Thainara Furforo <thaifurforo@gmail.com>
2022-10-24 17:12:22 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 27. Estrutura condicional (if-else)

--- ConditionalStructure/Program.cs
+++ ConditionalStructure/Program.cs
@@ -6,7 +6,33 @@ namespace ConditionalStrucutre
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Que horas são? Digite um número inteiro entre 0 e 23");
+            int hora = int.Parse(Console.ReadLine());
             
+            if (hora >= 18 || hora < 6)
+            {
+                Console.WriteLine("Boa noite");
+            }
+            else if (hora >= 12 && hora < 18)
+            {
+                Console.WriteLine("Boa tarde");
+            }
+            else
+            {
+                Console.WriteLine("Bom dia");    
+            }
+
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("Entre com um número inteiro");
+            int numero = int.Parse(Console.ReadLine());
+            
+            if (numero % 2 == 0)
+                Console.WriteLine("Este número é par!");
+            else
+                Console.WriteLine("Este número é ímpar!");
+
+
         }
     }
 }
