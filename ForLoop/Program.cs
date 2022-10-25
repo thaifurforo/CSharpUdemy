acc1e6b963298d576607e10221646752bc0da9c5
Thainara Furforo <thaifurforo@gmail.com>
2022-10-25 11:23:21 -0300

    Section 3: Recapitulação da Lógica de Programação usando C# 34. Estrutura repetitiva para (for)

---
+++ ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você irá digitar? ");
+            int n = int.Parse(Console.ReadLine());
+            int soma = 0; 
+            for (int x = 1; x <= n; x++)
+            {
+                Console.Write($"Digite o {x}º número inteiro: ");
+                soma += int.Parse(Console.ReadLine());
+            }
+
+            Console.Write($"Soma = {soma}");
+        }
+
+    }
+}
